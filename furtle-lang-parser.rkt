3f5470711d968aabfeae18fd932e79c10ec2e3b1
Sourav Datta <soura.jagat@gmail.com>
2018-02-09 14:02:19 +0530

    Update and rename furtle-lang-lexer.rkt to furtle-lang-parser.rkt

--- furtle-lang-parser.rkt
+++ furtle-lang-parser.rkt
@@ -4,13 +4,13 @@
 (require (prefix-in : parser-tools/lex-sre))
 
 
-(define-tokens TurtleTok [SYMBOL NUMBER OP])
-(define-empty-tokens TurtleTok* [TO END EOF NEWLINE REPEAT IF ELSE WHEN])
+(define-tokens FurtleTok [SYMBOL NUMBER OP])
+(define-empty-tokens FurtleTok* [TO END EOF NEWLINE REPEAT IF ELSE WHEN THEN])
 
-(define turtle-lexer (lexer
+(define furtle-lexer (lexer
                       [(eof) (token-EOF)]
                       ;;[#\newline (token-NEWLINE)]
-                      [(:or whitespace blank iso-control) (turtle-lexer input-port)]
+                      [(:or whitespace blank iso-control) (furtle-lexer input-port)]
                       [(:or (:: #\t #\o)
                             (:: #\T #\O)) (token-TO)]
                       [(:or (:: #\e #\n #\d)
@@ -23,17 +23,18 @@
                             (:: #\E #\L #\S #\E)) (token-ELSE)]
                       [(:or (:: #\w #\h #\e #\n)
                             (:: #\W #\H #\E #\N)) (token-WHEN)]
+                      [(:or (:: #\t #\h #\e #\n)
+                            (:: #\T #\H #\E #\N)) (token-THEN)]
                       [(:or #\+ #\- #\* #\/ #\< #\> #\= (:: #\& #\&) (:: #\| #\|) (:: #\! #\=) (:: #\: #\=) #\( #\)) (token-OP lexeme)]
                       [(:: alphabetic (:* (:or alphabetic numeric))) (token-SYMBOL (string->symbol lexeme))]
                       [(:+ numeric) (token-NUMBER (string->number lexeme))]))
 
 
-;; helper test function
 (define (string->tokens s)
   (let ([ip (open-input-string s)])
     (let looper ([tokens '()])
-      (let ([next-token (turtle-lexer ip)])
+      (let ([next-token (furtle-lexer ip)])
         (cond
          ((eq? next-token 'EOF) (reverse tokens))
          (else (looper (cons next-token tokens))))))))
- 
+
